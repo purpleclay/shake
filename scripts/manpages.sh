e00d61e296c18bbb05509da42b19d4791f3b5da1
purpleclay <purpleclaygh@gmail.com>
2022-06-05 08:58:03 +0100

    version and man pages command

---
+++ scripts/manpages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Borrowed from: https://raw.githubusercontent.com/goreleaser/goreleaser/main/scripts/manpages.sh
+set -e
+rm -rf manpages
+mkdir manpages
+go run . man | gzip -c -9 > manpages/shake.1.gz
